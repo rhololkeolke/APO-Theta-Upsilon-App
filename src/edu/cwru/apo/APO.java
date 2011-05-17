76e30eb3c4a2b06aa75b0074614da7eb14df8e70
Devin Schwab <digidevin@gmail.com>
2011-05-16 22:09:01 -0400

    Added a basic splash screen.  Added a basic skeleton for checking if logged in when app starts.  Added a login activity.  However, it seems to not display the layout correctly when the activity starts. This has to be investigated.  Once fixed, I need to add basic skeletons of other activites such as forgot password and home screen.

--- src/edu/cwru/apo/APO.java
+++ src/edu/cwru/apo/APO.java
@@ -1,31 +1,56 @@
 package edu.cwru.apo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import java.util.Date;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class APO extends Activity implements View.OnClickListener{
-	Button btn;
-	
+
+public class APO extends Activity {
+	 private static final int STOPSPLASH = 0;
+     //time in milliseconds
+     private static final long SPLASHTIME = 3000;
+     
+     private Boolean loggedIn = false; // hard coded to true until login check code is implemented
+    
+     //handler for splash screen
+     private Handler splashHandler = new Handler() {
+             @Override
+             public void handleMessage(Message msg) {
+                     switch (msg.what) {
+                     case STOPSPLASH:
+                             if(loggedIn) {
+                            	 // start main home activity
+                             }
+                             else {
+                            	 // start login activity
+                            	 Intent loginIntent = new Intent(APO.this, Login.class);
+                            	 APO.this.startActivity(loginIntent);
+                             }
+                             break;
+                     }
+                     super.handleMessage(msg);
+             }
+     };
+     
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        btn = new Button(this);
-        btn.setOnClickListener(this);
-        updateTime();
-        setContentView(btn);
-        //setContentView(R.layout.main);
-    }
+        // removes title bar on app, making image full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        setContentView(R.layout.splash_screen);
+        
+        // eventually there will be code here determining if the user is logged in or not
+        Message msg = new Message();
+        msg.what = STOPSPLASH;
+        splashHandler.sendMessageDelayed(msg, SPLASHTIME);
 
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		updateTime();
-	}
+    }
 	
-	private void updateTime() {
-		btn.setText(new Date().toString());
-	}
 }
---
+++ src/edu/cwru/apo/Login.java
@@ -0,0 +1,42 @@
+package edu.cwru.apo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends Activity implements OnClickListener{
+	private Button login_btn;
+	private Button forgot_btn;
+	private EditText username;
+	private EditText password;
+	
+	public void OnCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+		// map the login button to the layout
+		login_btn = (Button)findViewById(R.id.login);
+		login_btn.setOnClickListener(this);
+		// map the forgot password button to the layout
+		forgot_btn = (Button)findViewById(R.id.forgot_password);
+		forgot_btn.setOnClickListener(this);
+		
+	}
+
+	public void onClick(View v) {
+		switch(v.getId())
+		{
+		case R.id.login:
+			// check login credentials
+			break;
+		case R.id.forgot_password:
+			//start forgot password activity
+			break;
+		default:
+			//something went wrong add a throw here
+		}
+	}
+}
