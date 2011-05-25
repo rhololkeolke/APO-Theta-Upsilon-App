fc674d120f467de0a2fff66185005d369cb2fc22
Devin Schwab <digidevin@gmail.com>
2011-05-25 19:31:41 -0400

    Added a basic grid view that can display images.  Going to add the code to launch activities based on clicking on an image

--- gen/edu/cwru/apo/R.java
+++ gen/edu/cwru/apo/R.java
@@ -37,20 +37,29 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int transparent=0x7f050001;
         public static final int white=0x7f050000;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int splash_img=0x7f020001;
+        public static final int sample_0=0x7f020001;
+        public static final int sample_1=0x7f020002;
+        public static final int sample_2=0x7f020003;
+        public static final int sample_3=0x7f020004;
+        public static final int sample_4=0x7f020005;
+        public static final int sample_5=0x7f020006;
+        public static final int sample_6=0x7f020007;
+        public static final int sample_7=0x7f020008;
+        public static final int splash_img=0x7f020009;
     }
     public static final class id {
         public static final int forgot_password=0x7f070009;
+        public static final int gridView=0x7f070004;
         public static final int linearLayout1=0x7f070000;
         public static final int login=0x7f070008;
         public static final int password=0x7f070007;
         public static final int reset_btn=0x7f070003;
         public static final int splash=0x7f07000a;
-        public static final int tableLayout1=0x7f070004;
         public static final int textView1=0x7f070001;
         public static final int textView2=0x7f070006;
         public static final int username=0x7f070005;
--- src/edu/cwru/apo/Home.java
+++ src/edu/cwru/apo/Home.java
@@ -2,13 +2,26 @@ package edu.cwru.apo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 
-public class Home extends Activity {
+public class Home extends Activity implements OnItemClickListener{
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_screen);
+		
+		GridView gridview = (GridView) findViewById(R.id.gridView);
+		gridview.setAdapter(new ImageAdapter(this));
+		
+		gridview.setOnItemClickListener(this);
+	}
+
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
 	}
 
 }
